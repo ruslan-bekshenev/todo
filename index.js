f4d7576ba19990f2dc1a0801c9109854ecab6481
Ruslan <rbe@crtweb.ru>
2021-04-12 02:17:03 +0500

    верстка

--- index.js
+++ index.js
@@ -1,49 +1,70 @@
 class Todo {
   todo = []
+  idName = 'item'
 
   constructor() {
     this.todo = [
       {
+        id: 0,
         value: 'Покушать',
         isChecked: false
       },
       {
+        id: 1,
         value: 'Сходить в кино',
         isChecked: false
       },
       {
+        id: 2,
         value: 'Погладить кота',
         isChecked: false
       },
       {
+        id: 3,
         value: 'Поиграть в кс',
         isChecked: false
       },
       {
+        id: 4,
         value: 'Посмотреть кино',
         isChecked: false
       },
       {
+        id: 5,
         value: 'Напомнить себе какой лук не вкусный',
         isChecked: true
       },
     ]
   }
-
+  todoNode(item) {
+    return `
+      <li class="item">
+        <input type="checkbox" id="${this.idName}_${item.id}" ${item.isChecked && 'checked="checked"'}"/>
+        <label for="${this.idName}_${item.id}">
+          ${item.value}
+        </label>
+      </li>
+    `
+  }
   init() {
-    const list = document.getElementById('list')
-    const node = (item, id) => `<li class="item">
-          <input type="checkbox" id="item_${id}" ${item.isChecked && 'checked="checked"'}"/>
-          <label for="item_${id}">
-            ${item.value}
-          </label>
-        </li>`
-    this.todo.forEach((item, id) => {
-      list.innerHTML += node(item, id)
+    this.todo.forEach((item) => {
+      list.innerHTML += this.todoNode(item)
     })
+  }
+  itemCheckedChange(id) {
+    console.log('change')
+    const current = this.todo.find(item => item.id === id)
+    if (current) {
+      current.isChecked = !current?.isChecked
+      console.log(current)
+    }
+
+  }
+  itemValueChange(name) {
+    const current = this.todo.find(item => item.id === id)
+    current.value = name
   }
 }
 
 const toDo = new Todo()
 toDo.init()
-console.log(toDo.todo)
